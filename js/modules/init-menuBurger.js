ae19274dcb79b999f94c11053d48388dfb1ecc87
Valeria Rodionova <valeriya-off@yandex.ru>
2021-05-03 22:48:49 +0300

    Add styles for menu

--- js/modules/init-menuBurger.js
+++ js/modules/init-menuBurger.js
@@ -1,10 +1,11 @@
 const buttonMenu = document.querySelector('.menu__burger');
-const menuContent = document.querySelector('.menu__inner');
+const menuContent = document.querySelector('.menu');
 
 const initMenuBurger = () => {
   buttonMenu.addEventListener('click', () => {
     menuContent.classList.toggle('active');
     buttonMenu.classList.toggle('active');
+    document.body.classList.toggle('lock');
   })
 }
 
