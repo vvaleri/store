94e5cfb23529b2c48775970c9c08064aaef923cb
Valeria Rodionova <valeriya-off@yandex.ru>
2021-05-06 00:52:53 +0300

    Fix header on mobile

--- js/modules/init-header.js
+++ js/modules/init-header.js
@@ -1,15 +1,31 @@
 const header = document.querySelector('.header');
+const breakpoint = window.matchMedia('(max-width:768px)');
 
 const initHeader = () => {
-  window.addEventListener('scroll', () => { 
+
+  const isMobile = () => {
     let scrolled = document.documentElement.scrollTop;
 
-    if (scrolled > 60) {
+    if (scrolled > 10) {
       header.classList.add('fixed');
     } else {
       header.classList.remove('fixed')
     }
+  }
+
+  window.addEventListener('scroll', () => { 
+    let scrolled = document.documentElement.scrollTop;
+
+    if (breakpoint.matches) {
+      isMobile();
+    } else if (scrolled > 60) {
+      header.classList.add('fixed');
+    } else {
+      header.classList.remove('fixed');
+    }
   })
+
+  breakpoint.addEventListener('change', isMobile);
 }
 
 export { initHeader }
